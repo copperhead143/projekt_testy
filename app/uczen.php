b7a18fec383f3c72f2e0c375b66cb1c74dcc26fc
Julita <110465723+Drizzt-sama@users.noreply.github.com>
2024-01-11 09:15:17 +0100

    Dodaj informacje o uczniu

---
+++ app/uczen.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>uczen</title>
+</head>
+<body>
+    <?php
+        session_start();
+    ?>
+    <h2>Tutaj zrobic wybor testu</h2>
+    <a href="wybierz.php">Przejdz do wyboru testu</a>
+    <a href="wyloguj.php">Wyloguj</a>
+</body>
+</html>
