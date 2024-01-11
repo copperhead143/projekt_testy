b12193f900060246f945a908e35090ddb9fc5a12
szysiko2 <147710541+szysiko2@users.noreply.github.com>
2024-01-11 09:00:09 +0100

    Dodanie strony głównej dla prowadzącego

---
+++ app/panel_prowadzacego.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>panel prowadzace</title>
+</head>
+<body>
+    <body>
+    <div class="main">
+        <h1>Panel Prowadzącego</h1>
+        <a href="dodawanie_testu.php">Dodaj nowy test</a>
+        <a href="zmien_pytania.php">Zarządzaj pytaniami</a>
+        <a href="przegladaj_wyniki.php">Przeglądaj wyniki</a>
+        <a href="wyloguj.php">Wyloguj</a>
+    </div>
+</body>
+</body>
+</html>
